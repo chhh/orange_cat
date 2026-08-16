dab5fe31ec8f63df09945f3436f5f8f4efe29e60
Dmitry Avtonomov <dmt.avt@gmail.com>
2026-08-16 13:05:33 -0700

    snap two frames

--- main.py
+++ main.py
@@ -8,14 +8,17 @@ def main():
     k2 = "CAT_VIDEO_KEY_OUTSIDE" 
     api_key_inside = os.getenv(k1)
     api_key_outside = os.getenv(k2)
-    if not api_key:
-        raise RuntimeError(f"Env vars [{k1}, {k2}] not set. Either add it to .env file in this repo (it's gitignored or export it in your shell temporarily.")
+    if not api_key_inside or not api_key_outside:
+        raise RuntimeError(f"Env vars [{k1}, {k2}] not set. Add them to .env file (gitignored) or export in shell.")
 
-    # RTSPS_URL = f"rtsps://192.168.1.1:7441/{k1}?enableSrtp"
-    URL_INSIDE = f"rtsp://192.168.1.1:7447/{k1}?enableSrtp"
-    URL_OUTSIDE = f"rtsp://192.168.1.1:7447/{k2}?enableSrtp"
-    rtsps://192.168.1.1:7441/QgxffNeM11oM9m6D?enableSrtp
-    cap = cv2.VideoCapture(URL_INSIDE)
+    URL_INSIDE = f"rtsp://192.168.1.1:7447/{api_key_inside}?enableSrtp"
+    URL_OUTSIDE = f"rtsp://192.168.1.1:7447/{api_key_outside}?enableSrtp"
+    snap(URL_INSIDE, "frame_inside.jpg")
+    snap(URL_OUTSIDE, "frame_outside.jpg")
+
+
+def snap(url, filename):
+    cap = cv2.VideoCapture(url)
     if not cap.isOpened():
         raise RuntimeError("Could not open RTSP stream")
     ret, frame = cap.read()
@@ -24,8 +27,8 @@ def main():
         raise RuntimeError("Connected, but could not read a frame")
 
     print(f"Got frame: {frame.shape}")
-    cv2.imwrite("frame.jpg", frame)
-    print("Saved frame.jpg")
+    cv2.imwrite(filename, frame)
+    print(f"Saved {filename}")
     cap.release()
 
 if __name__ == "__main__":
